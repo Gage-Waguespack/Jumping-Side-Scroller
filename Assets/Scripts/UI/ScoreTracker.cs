1c9a8015092032a399172338b3c1d7b5db74e55f
Gage Waguespack <gagewaguespack@gmail.com>
2021-04-22 16:51:31 -0500

    Fixed nullptr exception

--- Assets/Scripts/UI/ScoreTracker.cs
+++ Assets/Scripts/UI/ScoreTracker.cs
@@ -8,6 +8,7 @@ public class ScoreTracker : MonoBehaviour
     [SerializeField]
     private Text _score;
 
+    //sets the score by turning the int into a string and modifying the score in game
     public void setScore(int Score)
     {
         _score.text = Score.ToString();
